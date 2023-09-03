3bd55904ddab641859ecef35c57cf14999e76ed8
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2023-09-03 18:14:49 +0300

    add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { getAllEntries } from "@/services/server/contentfulService";
 export default async function Home() {
   const entries = await getAllEntries();
   return (
-    <main>
+    <main className="mt-16">
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
         {entries.items.map((i) => (
           <ProductCard key={i.fields.id as string} item={i} />
--- src/components/server/Navbar.tsx
+++ src/components/server/Navbar.tsx
@@ -1,10 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="w-full border-b border-gray-600 py-4 px-2 md:px-8">
+    <nav className="fixed top-0 left-0 bg-white w-full border-b border-gray-600 py-4 px-2 md:px-8 opacity-90 z-40">
       <div className="flex justify-between">
-        <Link href={"/"}>BeMinimal</Link>
+        <Link href={"/"}>
+          <Image
+            src={"/logo/logo.png"}
+            alt="logo"
+            width={160}
+            height={160}
+          ></Image>
+        </Link>
         {/* <ul className="flex font-light text-gray-300 gap-4">
           <Link href={"/categories"}>Categories</Link>
           <Link href={"/brands"}>Brands</Link>
