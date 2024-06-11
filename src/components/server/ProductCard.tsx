2475f172926a2bad5016d051da56935ed60eacf2
Yağızhan Necat Yakalı <yagizhanyakali@gmail.com>
2024-06-11 13:08:15 +0300

    chore: Update ProductCard component layout and styles

--- src/components/server/ProductCard.tsx
+++ src/components/server/ProductCard.tsx
@@ -10,7 +10,7 @@ export default function ProductCard({ item }: PageProps) {
     <Link
       href={item.fields.productUrl}
       target="_blank"
-      className="group p-10 m-10 rounded-xl border border-gray-300 shadow-md transition-colors duration-300 bg-gray-100 hover:border-gray-800 hover:bg-white"
+      className="group flex flex-col justify-between p-10 m-10 rounded-xl border border-gray-300 shadow-md transition-colors duration-300 bg-gray-100 hover:border-gray-800 hover:bg-white"
     >
       <Image
         src={item.fields.productImage.fields.file.url.replace("//", "https://")}
@@ -20,14 +20,16 @@ export default function ProductCard({ item }: PageProps) {
         className="transition duration-300 ease-in-out group-hover:-translate-y-1.5"
         loading="eager"
       ></Image>
-      <div className="mt-4">
-        <p className="font-light text-gray-400">
-          {item.fields.brand} &middot; {item.fields.category}
-        </p>
-      </div>
-      <div className="flex justify-between mt-1 font-light text-gray-500">
-        <p>{item.fields.title}</p>
-        <p>{item.fields.price}</p>
+      <div>
+        <div className="mt-4">
+          <p className="font-light text-gray-400">
+            {item.fields.brand} &middot; {item.fields.category}
+          </p>
+        </div>
+        <div className="flex justify-between mt-1 font-light text-gray-500">
+          <p>{item.fields.title}</p>
+          <p>{item.fields.price}</p>
+        </div>
       </div>
     </Link>
   );
